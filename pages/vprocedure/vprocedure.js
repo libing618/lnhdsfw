fa843c50723746b0f0723e9dba21a78deb3e2241
libing618 <997098144@qq.com>
2018-03-24 11:40:16 +0800

    18030032

--- pages/vprocedure/vprocedure.js
+++ pages/vprocedure/vprocedure.js
@@ -5,20 +5,21 @@ Page({
   data:{
     uEV: app.globalData.user.emailVerified,
     enUpdate: false,
+    pno:'goods',
     vData: {},
     reqData: []
   },
-  pno:'articles',
+
   inFamily:false,
 
   onLoad: function(options) {
     var that = this ;
     let cUnitName = app.globalData.user.emailVerified ? app.roleData.uUnit.uName : '体验用户';     //用户已通过单位和职位审核
-    that.pno = options.pNo;
+    that.data.pno = options.pNo;
     let artid = Number(options.artId);
-    let pClass = require('../../model/procedureclass.js')[that.pno];
+    let pClass = require('../../model/procedureclass.js')[that.data.pno];
     that.inFamily = (typeof pClass.afamily != 'undefined');
-    that.data.vData = app.aData[that.pno][options.artId];
+    that.data.vData = app.aData[that.data.pno][options.artId];
     readShowFormat(pClass.pSuccess, that.data.vData).then(req=>{
       that.data.reqData=req;
       that.data.enUpdate = that.data.vData.unitId==app.roleData.uUnit.objectId && typeof pClass.suRoles!='undefined';  //本单位信息且流程有上级审批的才允许修改
@@ -31,15 +32,15 @@ Page({
 
   fEditProcedure: function(e){
     var that = this;
-    var url='/inputedit/fprocedure/fprocedure?pNo='+that.pno;
+    var url='/inputedit/fprocedure/fprocedure?pNo='+that.data.pno;
     switch (e.currentTarget.id){
       case 'fModify' :
         url += '&artId='+that.data.vData.objectId;
         break;
       case 'fTemplate' :
         url += that.inFamily ? '&artId='+that.data.vData.afamily : '';
-        let newRecord = that.inFamily ? that.pno+that.data.vData.afamily : that.pno;
-        app.aData[that.pno][newRecord] = that.data.vData;
+        let newRecord = that.inFamily ? that.data.pno+that.data.vData.afamily : that.data.pno;
+        app.aData[that.data.pno][newRecord] = that.data.vData;
         break;
     };
     wx.navigateTo({ url: url});
